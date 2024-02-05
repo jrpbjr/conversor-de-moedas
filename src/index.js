3c95199607a67b805d86ea419142763f9f2433e3
Jose Roberto Paes Barbosa Junior <jrpbjr@gmail.com>
2024-02-05 16:01:45 -0300

    adicionando o bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ConversorMoedas from './Conversor-moedas';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<ConversorMoedas />, document.getElementById('root'));
 
