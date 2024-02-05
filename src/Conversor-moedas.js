32f0ad19ad762d27915ab2166943eb8cd51c7a59
Jose Roberto Paes Barbosa Junior <jrpbjr@gmail.com>
2024-02-05 16:52:33 -0300

    Formulario html do conversor

--- src/Conversor-moedas.js
+++ src/Conversor-moedas.js
@@ -7,7 +7,42 @@ import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 function ConversorMoedas() {
   return (
-    <h1>Conversor de moedas</h1>
+    <div>
+      <h1>Conversor de moedas</h1>
+      <Jumbotron>
+        <Form>
+          <Form.Row>
+            <Col sm= "3">
+              <Form.Control placeholder='0' value={1} required />
+            </Col>
+
+            <Col sm= "3">
+              <Form.Control as="select">
+                </Form.Control>
+            </Col>
+
+            <Col sm= "1" className='text-center' style={{paddingTop:'5px'}}>
+              <FontAwesomeIcon icon={faAngleDoubleRight} />
+            </Col>
+
+            <Col sm= "3">
+            <Form.Control as="select">
+                </Form.Control>
+            </Col>
+
+            <Col sm= "2">
+              <Button variant='success' type='submit'>
+                <Spinner animation='border' size='sm'/>
+                Converter
+              </Button>
+            </Col>          
+            
+            
+          </Form.Row>
+        </Form>
+      </Jumbotron>
+    </div>
+    
   );
 }
 
