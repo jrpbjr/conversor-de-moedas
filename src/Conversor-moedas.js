e5daf579350745e17faeb9691136b514edeabaa1
Jose Roberto Paes Barbosa Junior <jrpbjr@gmail.com>
2024-02-05 16:08:42 -0300

    import de componentes iniciais

--- src/Conversor-moedas.js
+++ src/Conversor-moedas.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Conversor-moedas.css';
+import { Jumbotron, Button, Form, Col, Spinner} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+
 
 function ConversorMoedas() {
   return (
