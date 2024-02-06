3b6675ab1953ff6d7c4c781bb00ca4544af62c93
Jose Roberto Paes Barbosa Junior <jrpbjr@gmail.com>
2024-02-05 21:47:59 -0300

    adicionando HTML da modal

--- src/Conversor-moedas.js
+++ src/Conversor-moedas.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Conversor-moedas.css';
-import { Jumbotron, Button, Form, Col, Spinner, Alert} from 'react-bootstrap';
+import { Jumbotron, Button, Form, Col, Spinner, Alert, Modal} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
@@ -43,6 +43,21 @@ function ConversorMoedas() {
             
           </Form.Row>
         </Form>
+        <Modal show={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Conversão</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Resultado da conversão aqui ...
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant='success'>
+              Nova conversão
+            </Button>
+          </Modal.Footer>
+
+
+        </Modal>
       </Jumbotron>
     </div>
     
